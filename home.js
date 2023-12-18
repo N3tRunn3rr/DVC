a14efbe4e6a09fa774bd086df5dbbd89f53349ba
Kenji Fleming <kwf1999@gmail.com>
2023-12-18 14:43:54 -0500

    basic video inputs

---
+++ home.js
@@ -0,0 +1,12 @@
+// JS for video playing
+var myVideo = document.getElementById("myVid");
+function playVideo() { 
+ myVideo.play(); 
+} 
+function pauseVideo() { 
+ myVideo.pause(); 
+} 
+
+// $('video').click(function(){
+//     this[this.paused ? 'play' : 'pause']();
+// })
