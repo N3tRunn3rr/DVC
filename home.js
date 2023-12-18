ff1cf7ae55085cc47d4c9552f3cf1a0ac56237e5
Kenji Fleming <kwf1999@gmail.com>
2023-12-18 14:47:32 -0500

    fuck

--- home.js
+++ home.js
@@ -1,12 +1,16 @@
 // JS for video playing
-var myVideo = document.getElementById("myVid");
-function playVideo() { 
- myVideo.play(); 
-} 
-function pauseVideo() { 
- myVideo.pause(); 
-} 
+// var myVideo = document.getElementById("myVid");
+// function playVideo() { 
+//  myVideo.play(); 
+// } 
+// function pauseVideo() { 
+//  myVideo.pause(); 
+// } 
 
-// $('video').click(function(){
-//     this[this.paused ? 'play' : 'pause']();
-// })
+$('#myVid').click(function () {
+    if (myVideo.paused) {
+        myVideo.play();
+    } else {
+        myVideo.pause();
+    }
+})
