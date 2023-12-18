7ec25b4331859273644b93ba1b80c707ce208632
boy-son <59755443+boy-son@users.noreply.github.com>
2023-12-18 15:00:00 -0500

    can now click on video itself to pause and play

--- home.js
+++ home.js
@@ -1,16 +1,19 @@
-// JS for video playing
-// var myVideo = document.getElementById("myVid");
-// function playVideo() { 
-//  myVideo.play(); 
-// } 
-// function pauseVideo() { 
-//  myVideo.pause(); 
-// } 
-
-$('#myVid').click(function () {
-    if (myVideo.paused) {
-        myVideo.play();
-    } else {
-        myVideo.pause();
+$(document).ready(function() {
+    var myVideo = document.getElementById("myVid");
+  
+    function playVideo() { 
+      myVideo.play(); 
     }
-})
+  
+    function pauseVideo() { 
+      myVideo.pause(); 
+    }
+  
+    $('#myVid').on('click', function () {
+      if (myVideo.paused) {
+        playVideo();
+      } else {
+        pauseVideo();
+      }
+    });
+  });
